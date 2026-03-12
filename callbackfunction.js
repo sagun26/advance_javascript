c96dfb3fe5980d2590dcd5a6ad404dcf78091d3f
sagun26 <sagunsigdel6@gmail.com>
2026-03-12 15:53:52 +0545

    this

--- callbackfunction.js
+++ callbackfunction.js
@@ -8,3 +8,13 @@ function saybye(){
     console.log("Goodbye!");
 }
 greet("sagun", saybye);
+
+// timeout function
+setTimeout(function(){
+    console.log("This message is displayed after 2 seconds.");
+},2000);
+
+const items=[1,2,3,4,5];
+items.forEach(function(item){
+    console.log(item);
+})
