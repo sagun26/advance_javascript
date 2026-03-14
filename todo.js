7374789df0c698c1485e87a7298f93683a5188ce
sagun26 <sagunsigdel6@gmail.com>
2026-03-14 19:52:02 +0545

    todolist

--- todo.js
+++ todo.js
@@ -1,18 +1,18 @@
 const taskInput=document.getElementById("taskInput");
 const addBtn=document.getElementById("addBtn");
 const taskList=document.getElementById("taskList");
-let task =[];
-
+let task = JSON.parse(localStorage.getItem("task"));
+ console.log(task)
 //render tasks
 
 function renderTask(){
     taskList.innerHTML=task.map((task,index)=>{
-        const{completed,text}=task;
+        const{completed,text,date}=task;
      return`
      <li>
         <input type="checkbox"${completed?"checked":""}
          onchange="toggleTask(${index})" />
-        <span> ${text} </span>
+        <span> ${text} (${date})</span>
          <button onclick="deleteTask(${index})">Delete</button>
     </li>
 
@@ -31,6 +31,7 @@ function toggleTask(index)
         }
         return item;
     })
+    saveTask();
     renderTask();
 }
 
@@ -40,6 +41,7 @@ function deleteTask(index)
     task=task.filter((item,i)=>{
         return i!=index;
     })
+    saveTask();
     renderTask();
 }
 
@@ -59,3 +61,5 @@ addBtn.addEventListener("click",function(){
 function saveTask(){
     localStorage.setItem("task",JSON.stringify(task));
 }
+
+renderTask()
