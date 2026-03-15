b4848e690de52058f1e704fc733d86a5194ca37e
sagun26 <sagunsigdel6@gmail.com>
2026-03-15 15:10:30 +0545

    callback

---
+++ async/callback.js
@@ -0,0 +1,29 @@
+// function greetuser(username,callback)
+// {
+//     console.log("Hello"+username);
+//     callback();
+// }
+// function saybye(){
+//     console.log("goodbye");
+
+// }
+// greetuser("sagun",saybye);
+function loginUser(username,callback){
+    console.log(username);
+    setTimeout(() => {
+       callback("saguna"); 
+    },3000);
+}
+
+loginUser("sagun",function(username){
+    console.log("hello "+username);
+
+    getUserData(username , function(data){
+        console.log(data);
+    
+        getuserpost(data, function(post){
+            console.log(post);
+
+    })
+})
+});
