bd289d95349da97bec1e6320f5173aa56ca6bdef
sagun26 <sagunsigdel6@gmail.com>
2026-03-15 14:21:42 +0545

    asyncjs

---
+++ async/script.js
@@ -0,0 +1,10 @@
+// console.log("input");
+// console.log("processing");
+// console.log("output");
+
+//asynchrous example
+console.log("input");
+setTimeout(()=>{
+    console.log("processing");
+},2000);
+console.log("output");
