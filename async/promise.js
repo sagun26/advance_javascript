6659a4af0ecd4ce10727c887051717cfd51ca453
sagun26 <sagunsigdel6@gmail.com>
2026-03-16 14:31:57 +0545

    promise

--- async/promise.js
+++ async/promise.js
@@ -1,16 +1,29 @@
-const orderFood= new Promise(function(resolve,reject){
- const isAvaliable=false
-
-  if(isAvaliable){
- resolve("food is avaliable")
+const orderFood = new Promise(function(resolve, reject){
+const isAvaliable=true;
+if(isAvaliable){
+    resolve("food is avaliable")
+  }else{
+    reject("food is not avaliable")
  }
-  else {
- reject("food is not avaliable")
-}})
+ })
+ // console.log(typeof orderFood);
+ orderFood.then(message=>console.log(message)).catch(error=> console.log(error))  
 
-// orderFood.then(function(message){
-//     console.log(message)
-// })
-orderFood.then(message=> console.log(message)).catch(error=> console.log(error))
+ loginUser("sagun",function(username){
+    console.log("hello "+username);
 
+    getUserData(username , function(data){
+        console.log(data);
+    
+        getuserpost(data, function(post){
+            console.log(post);
 
+    })
+})
+})
+ 
+ loginUser
+    .then(getUserData)
+    .then(getUserPosts)
+    .then(posts =>console.log(post))
+    .catch(error=> console.log(error))
