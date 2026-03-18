d3a4f8f8934e9cdb795bda16927b26005bbe63fd
sagun26 <sagunsigdel6@gmail.com>
2026-03-18 12:22:08 +0545

    datatypes

---
+++ js/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log(3+3);
+console.log("hello"+"sagun");
+
+
+let name="sagun " //string 
+let age=12  //number
+let isLoggedIn=true //boolean
+
+
+//primitive data types  
+//number=> 2 ^53 (range)
+//bigint=> 2^1024 (range)
+//string=> "" or '' or ``
+//boolean=> true or false
+//null=> standalone value
+//undefined=> default value of uninitialized variable 
+//symbol=> unique identifier
+
+//object 
+console.log(typeof null)
