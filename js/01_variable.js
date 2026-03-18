78a22cc2971de5d208d24e57ed6578a3e6124ee4
sagun26 <sagunsigdel6@gmail.com>
2026-03-18 17:32:47 +0545

    javascript

---
+++ js/01_variable.js
@@ -0,0 +1,12 @@
+const accountId =12345
+let accountEmail="sagun@email.com"
+var accountPassword="54321"
+accountCity="pokhara"
+
+//accountId=2
+
+accountEmail="hello@gmail.com"
+accountPassword="23451"
+accountCity="kathmandu"
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity])
