130ae7565208069c75aba3e53b3a6d77683cd4f0
sagun26 <sagunsigdel6@gmail.com>
2026-03-19 15:26:35 +0545

    prefix postfix

--- js/03_conversionOperation.js
+++ js/03_conversionOperation.js
@@ -1,7 +1,38 @@
-let score="33abc"
-console.log(typeof score);
-console.log(typeof(score));
+// let score="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+
+// let str1="hello"
+// let str2=" sagun"
+// let str3=str1+str2
+// console.log(str3);
+// console.log("1"+str2);
+
+
+
+// console.log("1"+2);//12 
+// console.log("1"-2);//-1
+// console.log("1"+2+2);//122
+// console.log(1+2+"1"); //31
+
+// let gameCounter=100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// gameCounter++;
+// console.log(gameCounter);
+
+let a=10
+console.log(++a)
+console.log(a)  
+b=10
+console.log(b++)
+console.log(b)
