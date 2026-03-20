8ca0cd73e56be5772382a5cdec283497bd5c9ee3
sagun26 <sagunsigdel6@gmail.com>
2026-03-20 15:34:39 +0545

    comparision

--- js/04_comparision.js
+++ js/04_comparision.js
@@ -1,5 +1,23 @@
-console.log("2">1);
-console.log("02">2);
+// console.log("2">1);
+// console.log("02">2);
 
-console.log(null>0)
- 
+// console.log(null>0)
+//   const score=100
+//   const scorevalue=100.3
+//   const isLoggedIn=false
+//   const outsideTemp=null
+//   let userEmail;
+//   const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id===anotherId);
+
+const fruits=["apple","banana","grapes"]
+let myobj={
+    username:"sagun",
+    age:22,
+}
+
+
+const myFunction= function (){
+    console.log("hello world");
+}
