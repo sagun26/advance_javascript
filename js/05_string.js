c1563902472c9b0c2519c6799f334599a6064c9d
sagun26 <sagunsigdel6@gmail.com>
2026-03-21 18:13:20 +0545

    string

---
+++ js/05_string.js
@@ -0,0 +1,11 @@
+const name ="sagun"
+const repoCOunt=50
+//console.log(name+repoCOunt+"value");
+ console.log(`hello my name is ${name}`); 
+
+ const  gameName= new String("saguna")
+    console.log(gameName);
+    console.log(gameName.__proto__);
+
+    console.log(gameName.length());
+    console.log(gameName.toUpperCase());
