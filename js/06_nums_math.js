06f091180131f748975d013203aeba0ee5748eb7
sagun26 <sagunsigdel6@gmail.com>
2026-03-22 15:02:48 +0545

    num_math

---
+++ js/06_nums_math.js
@@ -0,0 +1,33 @@
+// const score=400
+// const balance= new Number (1000)
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+//  const otherNumber =2682.222
+//  console.log(otherNumber.toPrecision(3));
+
+//  const hundreds=1000000
+//     console.log(hundreds.toLocaleString('en-IN'));
+
+// Maths
+
+// console.log(Math)
+// console.log(Math.PI)
+// console.log(Math.E)     
+// console.log(Math.round(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.ceil(4.2)) 
+// console.log(Math.sqrt(16))
+// console.log(Math.pow(2,3))
+// console.log(Math.abs(-5))
+console.log((Math.random() * 10) + 1)
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1) +min))
+
+
