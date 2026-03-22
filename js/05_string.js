b5c3dd640df8fa0dc9ac054f8ef4952926a7d3c4
sagun26 <sagunsigdel6@gmail.com>
2026-03-22 14:38:23 +0545

    string

--- js/05_string.js
+++ js/05_string.js
@@ -1,11 +1,31 @@
-const name ="sagun"
-const repoCOunt=50
-//console.log(name+repoCOunt+"value");
- console.log(`hello my name is ${name}`); 
+// const name ="sagun"
+// const repoCOunt=50
+// //console.log(name+repoCOunt+"value");
+//  console.log(`hello my name is ${name}`); 
 
  const  gameName= new String("saguna")
     console.log(gameName);
     console.log(gameName.__proto__);
+     console.log(gameName.length);
 
-    console.log(gameName.length());
     console.log(gameName.toUpperCase());
+
+    console.log(gameName.charAt(2));
+      console.log(gameName.indexOf("a"));
+
+      const newString =gameName.substring(0,4)
+      console.log(newString);
+      const anotherString =gameName.slice(-8,4)
+      console.log(anotherString);
+
+
+      const newStringOne="  sagun  "
+      console.log(newStringOne);
+      console.log(newStringOne.trim());
+
+const url ="https://sagun.com/sagun%20sigdel"
+
+console.log(url.replace("%20", " "))
+console.log(url.includes('sigdel'))
+
+console.log(gameName.split("-"))
