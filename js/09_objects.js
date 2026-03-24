46bb002253867fc3fd160d95f49718e510fe46b5
sagun26 <sagunsigdel6@gmail.com>
2026-03-24 14:27:25 +0545

    object

---
+++ js/09_objects.js
@@ -0,0 +1,13 @@
+const user ={
+    name: 'saguna',
+    age: 30,
+    location:"pokhara",
+    email:"sagun@gmail.com",
+    courses: ['html', 'css', 'js'],
+}
+
+//
+console.log(user["name"]);
+console.log(user.age);
+  
+
