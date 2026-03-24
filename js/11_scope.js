7ea7f89484ba3c12c920df50ee7bf567d894480b
sagun26 <sagunsigdel6@gmail.com>
2026-03-24 14:52:39 +0545

    scope

---
+++ js/11_scope.js
@@ -0,0 +1,10 @@
+function one (){
+    const username="saguna"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+one()   
