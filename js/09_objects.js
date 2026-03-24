ac3e953a084dbca889c5f5ee163614a6efd8316f
sagun26 <sagunsigdel6@gmail.com>
2026-03-24 14:46:10 +0545

    function

--- js/09_objects.js
+++ js/09_objects.js
@@ -1,13 +1,12 @@
-const user ={
-    name: 'saguna',
-    age: 30,
-    location:"pokhara",
-    email:"sagun@gmail.com",
-    courses: ['html', 'css', 'js'],
-}
+// const user ={
+//     name: 'saguna',
+//     age: 30,
+//     location:"pokhara",
+//     email:"sagun@gmail.com",
+//     courses: ['html', 'css', 'js'],
+// }
 
-//
-console.log(user["name"]);
-console.log(user.age);
+// console.log(user["name"]);
+// console.log(user.age);
   
 
---
+++ js/10_functions.js
@@ -0,0 +1,10 @@
+function addnumbers(number1,number2){
+    return number1+number2;
+}
+const result=addnumbers(3,5)
+console.log(result)
+
+function loginuser(username){
+    return`${username} just logged in `
+}
+console.log(loginuser("saguna"))
