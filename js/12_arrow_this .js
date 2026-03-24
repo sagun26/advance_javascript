694e6954d0edac03eb1760ee0eb9d41c810efa58
sagun26 <sagunsigdel6@gmail.com>
2026-03-24 15:27:53 +0545

    arrow

---
+++ js/12_arrow_this .js
@@ -0,0 +1,25 @@
+// const user={
+//     username:"sagun",
+//     price:99,
+
+//     welcomeMessage:function(){
+//         console.log(`welcome ${this.username} to our website`)
+//         }
+// }
+// user.welcomeMessage()
+// user.username="saguun";
+// user.welcomeMessage()
+
+// const  user=()=>{
+//     let username="saguna"
+//     console.log(this.username);
+// }
+// user()
+
+const addTwo=(num1,num2)=>{
+    return num1+num2
+}
+console.log(addTwo(3,5))
+
+const addOne=(a,b)=>a+b
+console.log(addOne(4,6))
